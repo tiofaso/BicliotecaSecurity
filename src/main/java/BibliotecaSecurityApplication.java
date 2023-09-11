e3766e437032f4ac3e246d7d3aea1ff44119ac4f
Fábio Luiz Sousa Vaz <fabio.vaz@zup.com.br>
2023-09-11 15:55:22 -0300

    :construction: Classe principal para poder rodar o programa

---
+++ src/main/java/BibliotecaSecurityApplication.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+
+public class BibliotecaSecurityApplication {
+    public static void main(String[] args) { //Classe executável para rodar o programa
+        SpringApplication.run(BibliotecaSecurityApplication.class, args);
+    }
+
+}
