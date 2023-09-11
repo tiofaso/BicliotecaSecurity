9091a39ddea5e28ff55be88f3c6afecf09bad5c3
Fábio Luiz Sousa Vaz <fabio.vaz@zup.com.br>
2023-09-11 17:10:12 -0300

    :bug: Repositório corrigido para os padrões do Spring

--- src/main/java/Repository/LivroRepository.java
+++ src/main/java/Repository/LivroRepository.java
@@ -2,10 +2,11 @@ package Repository;
 
 import Model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
 
 }
