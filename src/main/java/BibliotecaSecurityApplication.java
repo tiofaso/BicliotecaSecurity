6820181840c41744315d6a8cb56e036dd7a6103f
Fábio Luiz Sousa Vaz <fabio.vaz@zup.com.br>
2023-09-11 17:51:14 -0300

    :bug: Reconfiguração para seguir a estrutura do Spring

--- src/main/java/BibliotecaSecurityApplication.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.boot.SpringApplication;
-
-public class BibliotecaSecurityApplication {
-    public static void main(String[] args) { //Classe executável para rodar o programa
-        SpringApplication.run(BibliotecaSecurityApplication.class, args);
-    }
-
-}
